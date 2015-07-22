f4427135f327e4aed03ffb4921177a802c16270a
bizzoxiv <bizzoxiv@users.noreply.github.com>
2015-07-22 22:20:43 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -84,10 +84,10 @@
 					updated = updated.parseISO(datastream.at);
 					var diff = null;
 					if(duration == '6hours') diff = 21600000;
-					// if(duration == '1day') diff = 86400000;
-					// if(duration == '1week') diff = 604800000;
-					// if(duration == '1month') diff = 2628000000;
-					// if(duration == '90days') diff = 7884000000;
+					if(duration == '1day') diff = 86400000;
+					if(duration == '1week') diff = 604800000;
+					if(duration == '1month') diff = 2628000000;
+					if(duration == '90days') diff = 7884000000;
 					then.setTime(now.getTime() - diff);
 					if(updated.getTime() > then.getTime()) {
 						if(datastreamIds && datastreamIds != '' && datastreamIds.indexOf(datastream.id) >= 0) {
